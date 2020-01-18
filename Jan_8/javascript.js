c37325350101590246572307c51a3bd46589e570
Quinn Calhoun <quinn.tcalhoun@gmail.com>
2020-01-18 02:06:30 -0800

    Updates to html code, minor updates to css, added the beginnings of js

---
+++ Jan_8/javascript.js
@@ -0,0 +1,22 @@
+document.querySelector('#easyPass').addEventListener('click', function(){
+    var passLength = 8
+    var password = []
+    var usables = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',0,1,2,3,4,5,6,7,8,9]
+    console.log(usables)
+        for (i = 1; i <= passLength; i++) {
+            var digit = Math.floor(Math.random() * 36)
+            console.log(digit)
+            password.push(digit)
+
+        }
+    var realPass = []
+    console.log(password)
+        for (i = 0; i < password.length; i++) {
+            var pleaseGod = usables[password[i]]
+            console.log(pleaseGod)
+            realPass.push(pleaseGod)
+        }
+    console.log(realPass)
+    console.log(realPass.join(''))
+    alert('Your Password is ' + realPass.join(''))
+    })  
